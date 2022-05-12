875db134b36b4b2ffabfd3f7212aa64d6408c341
Nilaan Gunabalachandran <quic_ngunabal@quicinc.com>
2022-05-12 10:28:46 -0400

    disp: msm: sde: add pineapple mdss version support
    This change adds pineapple mdss revision and enables features
    based on the hardware capabilities.
    
    Change-Id: I930e1ffd8e070f5bd258e5d3e441a966ade5f760
    Signed-off-by: Nilaan Gunabalachandran <quic_ngunabal@quicinc.com>

--- msm/sde/sde_hw_catalog.h
+++ msm/sde/sde_hw_catalog.h
@@ -53,6 +53,7 @@
 #define SDE_HW_VER_820	SDE_HW_VER(8, 2, 0) /* diwali */
 #define SDE_HW_VER_850	SDE_HW_VER(8, 5, 0) /* cape */
 #define SDE_HW_VER_900	SDE_HW_VER(9, 0, 0) /* kalama */
+#define SDE_HW_VER_A00	SDE_HW_VER(10, 0, 0) /* pineapple */
 
 /* Avoid using below IS_XXX macros outside catalog, use feature bit instead */
 #define IS_SDE_MAJOR_SAME(rev1, rev2)   \
@@ -82,6 +83,7 @@
 #define IS_DIWALI_TARGET(rev) IS_SDE_MAJOR_MINOR_SAME((rev), SDE_HW_VER_820)
 #define IS_CAPE_TARGET(rev) IS_SDE_MAJOR_MINOR_SAME((rev), SDE_HW_VER_850)
 #define IS_KALAMA_TARGET(rev) IS_SDE_MAJOR_MINOR_SAME((rev), SDE_HW_VER_900)
+#define IS_PINEAPPLE_TARGET(rev) IS_SDE_MAJOR_MINOR_SAME((rev), SDE_HW_VER_A00)
 
 #define SDE_HW_BLK_NAME_LEN	16
 
